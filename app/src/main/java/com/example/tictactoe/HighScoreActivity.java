147949f87330e46b4cda6d36dc1b2af87110a0e7
Simic Ivana, GHR-OSA-NEX-24 <Ivana.Simic@swisscom.com>
2023-03-07 15:22:54 +0100

    created HighScore Activity and started functions in MainActivity

---
+++ app/src/main/java/com/example/tictactoe/HighScoreActivity.java
@@ -0,0 +1,11 @@
+package com.example.tictactoe;
+
+// import android.widget.Button;
+
+// private Button HighScore;
+
+public class HighScoreActivity {
+
+
+    // HighScore=findViewById(R.id.HighScore);
+}
--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView button1,button2,button3,button4,button5,button6,button7,button8,button9;
+
+    private String playerOne = "X";
+    int b1=5,b2=5,b3=5,b4=5,b5=5,b6=5,b7=5,b8=5,b9=5;
+    private Button Reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1=findViewById(R.id.buttonImage1);
+        button2=findViewById(R.id.buttonImage2);
+        button3=findViewById(R.id.buttonImage3);
+        button4=findViewById(R.id.buttonImage4);
+        button5=findViewById(R.id.buttonImage5);
+        button6=findViewById(R.id.buttonImage6);
+        button7=findViewById(R.id.buttonImage7);
+        button8=findViewById(R.id.buttonImage8);
+        button9=findViewById(R.id.buttonImage9);
+
+
+        Reset=findViewById(R.id.Reset);
+
     }
 }
