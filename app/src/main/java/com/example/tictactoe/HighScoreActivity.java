b8ba1ffd649f8b25d120690d45744c388e29cbf7
Simic Ivana, GHR-OSA-NEX-24 <Ivana.Simic@swisscom.com>
2023-03-08 09:43:41 +0100

    Designd High Score Page

--- app/src/main/java/com/example/tictactoe/HighScoreActivity.java
+++ app/src/main/java/com/example/tictactoe/HighScoreActivity.java
@@ -9,13 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 
-
 public class HighScoreActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score);
 
     }
 }
